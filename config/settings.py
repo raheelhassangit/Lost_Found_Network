6fe96bd79d686c99255554016c67ca2473f9ce2e
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-08 14:30:43 +0500

    configurations

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     "tailwind",
     "theme",
+    "reports",
 ]
 
 if DEBUG:
@@ -135,14 +136,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR, 'base/static'
+    BASE_DIR / 'base' / 'static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
