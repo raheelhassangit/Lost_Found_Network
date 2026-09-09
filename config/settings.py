3759f7d9e3b54c83c8c16fe7c54e850355608cee
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-09 21:10:01 +0500

    Accounts urls configuration

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,10 @@ if DEBUG:
 
 ROOT_URLCONF = "config.urls"
 
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "accounts:login"
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
