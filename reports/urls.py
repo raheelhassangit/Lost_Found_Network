cb6654acb71dc90dbee21ce32c2bc7e40da96d47
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-16 16:06:27 +0500

    reports urls

--- reports/urls.py
+++ reports/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = "reports"
 
 urlpatterns = [
+    path("", views.report_list_view, name="list"),
+    path("mine/", views.my_reports_view, name="my_reports"),
     path("new/<str:report_type>/", views.report_create_view, name="create"),
+    path("<int:pk>/", views.report_detail_view, name="detail"),
     path("<int:pk>/edit/", views.report_update_view, name="update"),
     path("<int:pk>/delete/", views.report_delete_view, name="delete"),
-    path("<int:pk>/", views.report_detail_view, name="detail"),
 ]
