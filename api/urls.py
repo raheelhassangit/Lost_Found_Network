16b0352f76141fe5286fdce50a33a9d6b3168728
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-19 17:50:06 +0500

    api urls.py

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from reports.api_views import ReportViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register("reports", ReportViewSet, basename="report")
+router.register("categories", CategoryViewSet, basename="category")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
