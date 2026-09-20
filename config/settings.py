032347c038d7707afe729ff505f24fefccd4ee7a
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-20 14:36:46 +0500

    corsheaders & throtlling config

--- config/settings.py
+++ config/settings.py
@@ -182,6 +182,7 @@ PLATFORM_SUPPORT_EMAIL = config("PLATFORM_SUPPORT_EMAIL", default="")
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
