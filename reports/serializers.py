be36bcb868492c8567c4b03946f067ec729d887b
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-20 17:41:54 +0500

    Review API

--- reports/serializers.py
+++ reports/serializers.py
@@ -7,23 +7,25 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ["id", "name"]
 
-
-class ReportSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source="user.username")
-    category_name = serializers.ReadOnlyField(source="category.name")
-
-    class Meta:
-        model = Report
-        fields = [
-            "id", "report_type", "item_name", "description", "category", "category_name",
-            "color", "location", "date", "image", "status", "user", "created_at", "updated_at",
-        ]
-        read_only_fields = ["status", "user"]
-        
 class ReviewSerializer(serializers.ModelSerializer):
     reviewed_by = serializers.ReadOnlyField(source="reviewed_by.username")
 
     class Meta:
         model = Review
         fields = ["id", "report", "reviewed_by", "comment", "created_at"]
-        read_only_fields = ["report", "reviewed_by"]        
+        read_only_fields = ["report", "reviewed_by"] 
+
+class ReportSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.username")
+    category_name = serializers.ReadOnlyField(source="category.name")
+    review = ReviewSerializer(read_only=True)
+
+    class Meta:
+        model = Report
+        fields = [
+            "id", "report_type", "item_name", "description", "category", "category_name",
+            "color", "location", "date", "image", "status", "user", "review", "created_at", "updated_at",
+        ]
+        read_only_fields = ["status", "user"]
+        
+       
