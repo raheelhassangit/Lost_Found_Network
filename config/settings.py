f045de5291404eaef08431aff363741f4f5a9073
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-21 23:37:47 +0500

    debugging

--- config/settings.py
+++ config/settings.py
@@ -153,10 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'base' / 'static',
+    # BASE_DIR / 'base' / 'static',
 ]
 
 MEDIA_URL = '/media/'
